36ebb90578d66c4292b6b1304af5e175cc10ae2d
ssuxx <ssuxxarr@gmail.com>
2024-03-26 07:46:01 +0300

    added simple form

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,16 @@
+from flask import render_template, flash, redirect, url_for
 from app import app
-from flask import render_template
+from app.forms import LoginForm
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        print("VALID!")
+        flash(f'Login requested for user {form.username.data}, remember me = {form.remember_me.data}')
+        return redirect(url_for('index'))
+    return render_template("login.html", title="Sign In", form=form)
 
 
 @app.route("/")
