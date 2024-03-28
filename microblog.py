7a535c86f7ce1db86f31e606d26311f595e00c04
pavel.s <ssuxxarr@mail.ru>
2024-03-28 15:46:30 +0300

    added work with database and login, logout and registration form

--- microblog.py
+++ microblog.py
@@ -1,3 +1,9 @@
 """https://habr.com/ru/articles/346342/"""
 
-from app import app
+from app import app, db
+from app.models import User, Post
+
+
+@app.shell_context_processors
+def make_shell_context():
+    return {"db": db, "User": User, "Post": Post}
